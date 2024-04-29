2fc6f3fc23cd76adc5af994d9f2c90551ae26dbb
Samudra-Ekanayake <samudra2724@gmail.com>
2024-04-29 15:55:12 +0200

    M3

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let = listaSpesa [
     "merendine"
 ];
 
+
 for (let i = 0; i < listaSpesa.length; i++) {
     console.log(listaSpesa [i])
 }
@@ -14,4 +15,15 @@ while (i < listaSpesa.length) {
     console.log(listaSpesa [i])
 }
 
+
+const ul = document.getElementById("listaSpesa");
+let b = 0
+
+while (b < listaSpesa.length) {
+    const li = document.createElement("li;");
+    li.textContent = listaSpesa[b];
+    ul.appendChild(li);
+    b++
+    
+}
 
