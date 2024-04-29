33ce6ed9c41139bf58978175e1651a33cff67f3d
Samudra-Ekanayake <samudra2724@gmail.com>
2024-04-29 10:10:21 +0200

    while

---
+++ main.js
@@ -0,0 +1,16 @@
+let = listaSpesa [
+    "pomodori", "limone",
+    "arachidi", "noci",
+    "merendine"
+];
+
+for (let i = 0; i < listaSpesa.length; i++) {
+    console.log(listaSpesa [i])
+}
+
+let i = 0
+
+while (i < listaSpesa.length) {
+    console.log(listaSpesa [i])
+}
+
